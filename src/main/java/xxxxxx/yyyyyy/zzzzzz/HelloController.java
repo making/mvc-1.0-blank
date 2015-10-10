5237a3612bce3112262f912a684794f780ed9277
Toshiaki Maki <makingx@gmail.com>
2015-10-10 23:40:16 +0900

    Fix package

--- src/main/java/xxxxxx/yyyyyy/zzzzzz/HelloController.java
+++ src/main/java/xxxxxx/yyyyyy/zzzzzz/HelloController.java
@@ -3,7 +3,7 @@ package xxxxxx.yyyyyy.zzzzzz;
 import java.time.LocalDateTime;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.mvc.Controller;
+import javax.mvc.annotation.Controller;
 import javax.mvc.Models;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
