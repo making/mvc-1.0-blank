191f3e53d7a723ffea24cad075bc234ac65e135e
Toshiaki Maki <makingx@gmail.com>
2015-10-12 00:24:28 +0900

    Rewrite

--- src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
+++ src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
@@ -1,9 +1,17 @@
 package xxxxxx.yyyyyy.zzzzzz;
 
+import javax.mvc.security.Csrf;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashMap;
+import java.util.Map;
 
 @ApplicationPath("app")
 public class App extends Application {
-
+    @Override
+    public Map<String, Object> getProperties() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(Csrf.CSRF_PROTECTION, Csrf.CsrfOptions.EXPLICIT);
+        return props;
+    }
 }
